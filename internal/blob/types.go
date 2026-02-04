491734e19ce7ce26abfbd12a991a5341346d0391
Pedro Gomes <79362098+pedrolucas201@users.noreply.github.com>
2026-02-04 14:11:28 -0300

    chore: remove unused worker and blob handling code

--- internal/blob/types.go
+++
@@ -1,8 +0,0 @@
-package blob
-
-import "context"
-
-type Store interface {
-	Save(ctx context.Context, key string, data []byte) (string, error)
-	Load(ctx context.Context, path string) ([]byte, error)
-}
