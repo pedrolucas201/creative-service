e4131d2d11cc681d5289d1305eb1f76e263ce18a
Pedro Gomes <79362098+pedrolucas201@users.noreply.github.com>
2026-02-11 00:05:30 -0300

    feat: add missing handler methods to Handlers interface and update router routes

--- internal/httpapi/router.go
+++ internal/httpapi/router.go
@@ -8,13 +8,19 @@ import (
 
 type Handlers interface {
 	Health(http.ResponseWriter, *http.Request)
+	ListClients(http.ResponseWriter, *http.Request)
+	ListAdAccountsByClient(http.ResponseWriter, *http.Request)
 	CreateImageCreative(http.ResponseWriter, *http.Request)
 	CreateVideoCreative(http.ResponseWriter, *http.Request)
 	ListCreatives(http.ResponseWriter, *http.Request)
 	GetCreative(http.ResponseWriter, *http.Request)
+	SoftDeleteCreative(http.ResponseWriter, *http.Request)
 	CreateCampaign(http.ResponseWriter, *http.Request)
+	ListCampaigns(http.ResponseWriter, *http.Request)
 	CreateAdSet(http.ResponseWriter, *http.Request)
+	ListAdSets(http.ResponseWriter, *http.Request)
 	CreateAd(http.ResponseWriter, *http.Request)
+	ListAds(http.ResponseWriter, *http.Request)
 }
 
 func NewRouter(h Handlers) http.Handler {
@@ -22,13 +28,25 @@ func NewRouter(h Handlers) http.Handler {
 	r.Use(Recoverer, AccessLog)
 
 	r.Get("/v1/health", h.Health)
+	
+	// Clients & Ad Accounts
+	r.Get("/v1/clients", h.ListClients)
+	r.Get("/v1/clients/{client_uuid}/ad-accounts", h.ListAdAccountsByClient)
+	
+	// Creatives
 	r.Post("/v1/creatives/image", h.CreateImageCreative)
 	r.Post("/v1/creatives/video", h.CreateVideoCreative)
 	r.Get("/v1/creatives", h.ListCreatives)
 	r.Get("/v1/creatives/{creative_id}", h.GetCreative)
+	r.Delete("/v1/creatives/{creative_id}", h.SoftDeleteCreative)
+	
+	// Campaigns, AdSets, Ads
 	r.Post("/v1/campaigns", h.CreateCampaign)
+	r.Get("/v1/campaigns", h.ListCampaigns)
 	r.Post("/v1/adsets", h.CreateAdSet)
+	r.Get("/v1/adsets", h.ListAdSets)
 	r.Post("/v1/ads", h.CreateAd)
+	r.Get("/v1/ads", h.ListAds)
 
 	return r
 }
